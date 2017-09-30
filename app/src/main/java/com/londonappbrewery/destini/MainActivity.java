3f1b94fa434560528e1e80be401269277685416d
mshoaibluqman <mshoaibluqman@icloud.com>
2017-09-30 22:18:28 +0500

    Its Done.

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,28 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
 
+    TextView storyTextView;
+    Button buttonTop , buttonBottom;
+    int storyIndex;
+    String userSelection;
+    String trackStory;
+
+    StoryData[] storyDatas = new StoryData[]{
+            new StoryData(R.string.T1_Story, R.string.T1_Ans1, R.string.T1_Ans2),
+            new StoryData(R.string.T2_Story, R.string.T2_Ans1, R.string.T2_Ans2),
+            new StoryData(R.string.T3_Story, R.string.T3_Ans1, R.string.T3_Ans2)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +31,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
+
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        buttonTop = (Button) findViewById(R.id.buttonTop);
+        buttonBottom = (Button) findViewById(R.id.buttonBottom);
+
+        storyTextView.setText(storyDatas[storyIndex].getStory());
+        buttonTop.setText(storyDatas[storyIndex].getAns_1());
+        buttonBottom.setText(storyDatas[storyIndex].getAns_2());
+
+
+        buttonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                topButtonChangeStory();
+
+                userSelection = buttonTop.getText().toString();
+
+                trackStory = storyTextView.getText().toString();
+
+
+
+
+            }
+        });
+
+
+        buttonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                userSelection = buttonBottom.getText().toString();
+
+                bottomButtonChangeStory();
+
+            }
+        });
 
 
 
@@ -27,4 +83,81 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
+    private void topButtonChangeStory() {
+
+//        Toast.makeText(this, R.string.T2_Story, Toast.LENGTH_LONG).show();
+
+
+        storyIndex = storyIndex + 2;
+
+        if(storyIndex < storyDatas.length) {
+
+            storyTextView.setText(storyDatas[storyIndex].getStory());
+            buttonTop.setText(storyDatas[storyIndex].getAns_1());
+            buttonBottom.setText(storyDatas[storyIndex].getAns_2());
+        }
+        else{
+
+
+            topButtonCheckForStoryEnd();
+
+        }
+
+
+
+    }
+
+
+
+    private void topButtonCheckForStoryEnd() {
+
+
+        if(userSelection == buttonTop.getText().toString()) {
+            storyTextView.setText(R.string.T6_End);
+        }
+
+        else if(userSelection == buttonBottom.getText().toString()){
+            storyTextView.setText(R.string.T5_End);
+        }
+
+            buttonTop.setVisibility(View.GONE);
+            buttonBottom.setVisibility(View.GONE);
+
+    }
+
+
+
+    private void bottomButtonChangeStory() {
+
+        if (storyIndex == 1) {
+
+            storyTextView.setText(R.string.T4_End);
+            buttonTop.setVisibility(View.GONE);
+            buttonBottom.setVisibility(View.GONE);
+
+        } else {
+
+            storyIndex = storyIndex + 1;
+
+            Toast.makeText(this, "Clicked" + storyIndex, Toast.LENGTH_LONG).show();
+
+            if (storyIndex < storyDatas.length) {
+
+
+                storyTextView.setText(storyDatas[storyIndex].getStory());
+                buttonTop.setText(storyDatas[storyIndex].getAns_1());
+                buttonBottom.setText(storyDatas[storyIndex].getAns_2());
+            } else {
+
+                topButtonCheckForStoryEnd();
+            }
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/londonappbrewery/destini/StoryData.java
@@ -0,0 +1,43 @@
+package com.londonappbrewery.destini;
+
+/**
+ * Created by muhammadirfan on 30/09/2017.
+ */
+
+public class StoryData {
+
+    int Story;
+    int ans_1;
+    int ans_2;
+
+    public StoryData(int story, int ans_1, int ans_2){
+
+        this.Story = story;
+        this.ans_1 = ans_1;
+        this.ans_2 = ans_2;
+    }
+
+    public int getStory() {
+        return Story;
+    }
+
+    public void setStory(int story) {
+        Story = story;
+    }
+
+    public int getAns_1() {
+        return ans_1;
+    }
+
+    public void setAns_1(int ans_1) {
+        this.ans_1 = ans_1;
+    }
+
+    public int getAns_2() {
+        return ans_2;
+    }
+
+    public void setAns_2(int ans_2) {
+        this.ans_2 = ans_2;
+    }
+}
